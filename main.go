2c17024b36a67579b5104a66e712078954d3623f
Anirudhvunnam <anirudhvunnam94@gmail.com>
2023-11-20 15:57:44 -0500

    Committing local changes 1st reservation sample to main.go

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/yourusername/restaurant/api"
+)
 
 func main() {
-	fmt.Println("New Project for Restaurant")
+	r := mux.NewRouter()
+
+	// Define your API endpoints
+	r.HandleFunc("/api/reservation", api.AddReservationEndpoint).Methods("POST")
+
+	http.Handle("/", r)
+	http.ListenAndServe(":8080", nil)
 }
